767422110fdf9c503dd37b18ae711a49399cd92b
Matt Bush <matt.mb697@gmail.com>
2020-02-16 11:34:07 +0200

    print continuously to same line

--- morsePi.py
+++ morsePi.py
@@ -16,7 +16,7 @@ def timeout():
     global buffer
     if(buffer != ""):
         letter = morse_to_letter(buffer)
-        print letter,
+        print (letter,)
         buffer = ""
 
 t = Timer(2.0, timeout)
